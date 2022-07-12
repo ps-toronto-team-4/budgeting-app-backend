5a8ba3024b96bdcfc2c7098a2ce48e63dfbe12ec
Calvin Mozola <calvin@stuntfactory.com>
2022-07-12 16:09:04 -0400

    Added update to expense and linked other components

--- src/main/java/com/sapient/model/service/ExpenseService.java
+++ src/main/java/com/sapient/model/service/ExpenseService.java
@@ -1,8 +1,10 @@
 package com.sapient.model.service;
 
+import com.sapient.exception.CategoryNotFoundException;
 import com.sapient.exception.NotAuthorizedException;
 import com.sapient.exception.RecordNotFoundException;
 import com.sapient.exception.UserNotFoundException;
+import com.sapient.model.beans.Category;
 import com.sapient.model.beans.Expense;
 import com.sapient.model.beans.User;
 import com.sapient.model.dao.ExpenseRepository;
@@ -20,22 +22,61 @@ public class ExpenseService {
     @Autowired
     private UserService userService;
 
-    public Expense createExpense(String passwordHash, String title, String description, Double amount) throws NotAuthorizedException {
+	@Autowired
+	private CategoryService categoryService;
+
+    public Expense createExpense(String passwordHash, String title, String description, Double amount, Date date, Integer categoryId, Integer merchantId, Integer recurrenceId) throws NotAuthorizedException, RecordNotFoundException {
 		User foundUser;
+		Category foundCategory;
 		try {
 			foundUser = userService.getUserByPasswordHash(passwordHash);
+			foundCategory = categoryService.getCategory(passwordHash, categoryId);
 		} catch (UserNotFoundException e) {
-			throw new NotAuthorizedException();
+			throw new NotAuthorizedException("Not authorized: " + e.getMessage());
+		} catch (CategoryNotFoundException e) {
+			throw new RecordNotFoundException("Can't find category for '"+categoryId+"'");
 		}
 		Expense expense = new Expense();
-		expense.setDate( new Date(System.currentTimeMillis()) );
+		//TODO? do we want to auto add in the date to the current time?
+//		expense.setDate( new Date(System.currentTimeMillis()) );
+		expense.setDate(date);
         expense.setUser(foundUser);
     	expense.setTitle(title);
     	expense.setAmount(amount);
     	expense.setDescription(description);
+		expense.setCategory(foundCategory);
+		//TODO merchant
     	expenseDao.save(expense);
     	return expense;
     }
+
+	public Expense updateExpense(String passwordHash, Integer id, String title, String description, Double amount, Date date, Integer categoryId, Integer merchantId, Integer recurrenceId) throws NotAuthorizedException, RecordNotFoundException {
+		User foundUser;
+		Category foundCategory;
+		Expense expense = expenseDao.findById(id).orElse(null);
+		if(expense == null){
+			throw new RecordNotFoundException("Can not find Expense with ID of '"+id+"'");
+		}
+		if(!expense.getUser().getPasswordHash().equals(passwordHash)){
+			throw new NotAuthorizedException("Can not modify expense, doesn't not belong to you or aren't authenticated properly");
+		}
+		try {
+			foundUser = userService.getUserByPasswordHash(passwordHash);
+			foundCategory = categoryService.getCategory(passwordHash, categoryId);
+		} catch (UserNotFoundException | CategoryNotFoundException e) {
+			throw new RecordNotFoundException("Failed to find linked components or authentication to linked resources denied: " + e.getMessage());
+		}
+
+		expense.setDate(date);
+		expense.setUser(foundUser);
+		expense.setTitle(title);
+		expense.setAmount(amount);
+		expense.setDescription(description);
+		expense.setCategory(foundCategory);
+		//TODO merchant
+		expenseDao.save(expense);
+		return expense;
+	}
 
     public Expense deleteExpense(String passwordHash, Integer id) throws RecordNotFoundException, NotAuthorizedException {
     	Expense found = expenseDao.findById(id).orElse(null);
