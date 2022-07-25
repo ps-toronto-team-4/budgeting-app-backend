2fcdb28ec844b7044f5dfb9fed87387e9274357b
jere-mie <jeremiedevelops@gmail.com>
2022-07-25 14:48:33 -0400

    I42: Adding category and merchant insights

--- src/main/java/com/sapient/model/beans/MonthType.java
+++ src/main/java/com/sapient/model/beans/MonthType.java
@@ -12,5 +12,19 @@ public enum MonthType {
     SEPTEMBER,
     OCTOBER,
     NOVEMBER,
-    DECEMBER
+    DECEMBER;
+
+    public MonthYear previousMonth(Integer year){
+        if(this.ordinal() == 0){
+            return new MonthYear(this.values()[11], year-1);
+        }
+        return new MonthYear(this.values()[this.ordinal()-1], year);
+    }
+
+    public MonthYear nextMonth(Integer year){
+        if(this.ordinal() == 11){
+            return new MonthYear(this.values()[0], year+1);
+        }
+        return new MonthYear(this.values()[this.ordinal()+1], year);
+    }
 }
